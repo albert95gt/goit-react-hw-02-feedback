fb38c9f9d0728c0c69b155ff6df1c76e87627bd4
Albert Takács <albitakacs@gmail.com>
2022-01-09 19:24:36 +0100

    add feedback for app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,21 +1,71 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  getStateKeys = () => {
+    return Object.keys(this.state);
+  };
+  getStateEntries = () => {
+    return Object.entries(this.state);
+  };
+  incrementFeedbackBtn = key => {
+    this.setState(state => {
+      return {
+        [key]: state[key] + 1,
+      };
+    });
+  };
+  countTotalFeedback = () => {
+    const stateValue = Object.values(this.state);
+    const total = stateValue.reduce((acc, value) => acc + value);
+    return total;
+  };
+  countPositiveFeedbackPercentage = total => {
+    const totalPercenage = (this.state.good / total) * 100;
+    return Math.round(totalPercenage);
+  };
+
+  render() {
+    const stateKeys = this.getStateKeys();
+    const stateEntries = this.getStateEntries();
+    const total = this.countTotalFeedback();
+    const positiveFeedback = this.countPositiveFeedbackPercentage(total);
+    return (
+      <>
+        <h1>Please leave feedback</h1>
+        {stateKeys.map(key => {
+          return (
+            <button
+              key={key}
+              type="button"
+              onClick={() => this.incrementFeedbackBtn(key)}
+            >
+              {key}
+            </button>
+          );
+        })}
+
+        <h2>Statistics</h2>
+        <ul>
+          {stateEntries.map(([key, value]) => {
+            return (
+              <li key={key}>
+                {key}:<span>{value}</span>
+              </li>
+            );
+          })}
+        </ul>
+
+        <p>Total: {total}</p>
+
+        <p>Positive feedback: {positiveFeedback}%</p>
+      </>
+    );
+  }
 }
 
 export default App;
